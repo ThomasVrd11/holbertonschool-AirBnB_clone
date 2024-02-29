d7b4fe547b509a7e2478173c7df4f884983e17da
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-29 10:36:55 +0100

    base_model - added __init__ / __str__ / save / to_dict methods

--- models/base_model.py
+++ models/base_model.py
@@ -4,16 +4,31 @@ import datetime
 import uuid
 import models
 
+
 class BaseModel:
     """Defines all common attributes/methods for other classes
     attirbutes:
         id (str): unique id for each instance
         created_at (datetime): time instance was created
         updated_at (datetime): time instance was updated"""
-    
+
     def __init__(self, *args, **kwargs):
         """Initiates a new BaseModel instance
         args: non-keyworded arguments
         kwargs: keyworded arguments"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-        
+    def __str__(self):
+        return ("[{}] ({}) {}".format(__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        dict_copy = self.__dict__.copy()
+        dict_copy['__class__'] = self.__class__.__name__
+        dict_copy['created_at'] = dict_copy['created_at'].isoformat()
+        dict_copy['updated_at'] = dict_copy['updated_at'].isoformat()
+        return dict_copy
