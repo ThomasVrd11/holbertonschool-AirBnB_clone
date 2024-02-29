c69a276081b4667e707d9958eca1d8cb07b0b9a0
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 14:40:31 +0100

    Add Amenity class that inherits from BaseModel

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defines the Amenity class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """It is the amenity class that inherits from BaseModel.
+    It has the following attributes:
+    name: string - empty string
+    """
+    
+    name = ""
+    
