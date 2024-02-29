516bbafc15a27fe151a804d9bda79a45a9ed1e99
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 14:22:41 +0100

    Add Review and State classes to models

--- models/review.py
+++ models/review.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""Defines the Review class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """It is the review class that inherits from BaseModel.
+    It has the following attributes:
+    place_id: string - empty string: it will be the Place.id
+    user_id: string - empty string: it will be the User.id
+    text: string - empty string
+    """
+    place_id = ""
+    user_id = ""
+    text = ""
+    
--- models/state.py
+++ models/state.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Defines the State class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """It is the state class that inherits from BaseModel.
+    It has the following attributes:
+    name: string - empty string
+    """
+    name = ""
+    
