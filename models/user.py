96e01a955ee9e0f664e95629106474df798d3c92
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 14:21:33 +0100

    Add User class that inherits from BaseModel

--- models/user.py
+++ models/user.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Defines the User class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """It is the user class that inherits from BaseModel.
+    It has the following attributes:
+    email: string - empty string
+    password: string - empty string
+    first_name: string - empty string
+    last_name: string - empty string
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+    
