8f9e4125d027b89ea7b5ae7ff67d81235cceb998
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 14:17:28 +0100

    Add do_all and do_update methods to HBNBCommand class

--- console.py
+++ console.py
@@ -87,4 +87,55 @@ class HBNBCommand(cmd.Cmd):
                 del dict_objs[instance_key]
                 storage.save()
 
+    def do_all(self, arg):
+        """Prints all string representation of all instances based or not on the class name
+        it can be used with or without class name"""
+        dict_objs = storage.all()
+        list_objs = []
+        
+        if len(arg) == 0:
+            for keys, values in dict_objs.items():
+                list_objs.append(str(values))
+            print(list_objs)
+            
+        elif arg in self.class_listing:
+            for keys, values in dict_objs.items():
+                if values.__class__.__name__ == arg:
+                    list_objs.append(str(values))
+            print(list_objs)
 
+        else:
+            print("** class doesn't exist **")
+            
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id by adding or updating attribute"""
+        token = arg.split(" ")
+        dict_objs = storage.all()
+        
+        if len(arg) == 0:
+            print("** class name missing **")
+
+        elif token[0] not in self.class_list:
+            print("** class doesn't exist **")
+
+        elif len(token) < 2:
+            print("** instance id missing **")
+
+        else:
+            instance_key = "{}.{}".format(token[0], token[1])
+
+            if instance_key not in dict_objs.keys():
+                print("** no instance found **")
+
+            elif len(token) < 3:
+                print("** attribute name missing **")
+
+            elif len(token) < 4:
+                print("** value missing **")
+
+            else:
+                setattr(dict_objs[instance_key], token[2], token[3])
+                storage.save()
+                
+if __name__ == "__main__":
+    HBNBCommand().cmdloop()
