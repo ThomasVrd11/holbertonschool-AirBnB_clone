bda6e97f6dcfdc718ce8b56cbfb025588f32d196
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 09:16:00 +0100

    setup

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defines the Basemodel class that all other classes will inherit from"""
+import datetime
+import uuid
+import models
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes
+    attirbutes:
+        id (str): unique id for each instance
+        created_at (datetime): time instance was created
+        updated_at (datetime): time instance was updated"""
