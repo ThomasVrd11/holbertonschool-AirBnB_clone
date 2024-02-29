c5926d1ec037e89347f0efe8d91f5737c1ce6603
ThomasVrd11 <Thomasviard@orange.fr>
2024-02-29 14:37:41 +0100

    Add Place class to models

--- models/place.py
+++ models/place.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""Defines the Place class that inherits from BaseModel."""
+from models.base_model import BaseModel
+
+class Place(BaseModel):
+    """It is the place class that inherits from BaseModel.
+    It has the following attributes:
+    city_id: string - empty string: it will be the City.id
+    user_id: string - empty string: it will be the User.id
+    name: string - empty string
+    description: string - empty string
+    number_rooms: integer - 0
+    number_bathrooms: integer - 0
+    max_guest: integer - 0
+    price_by_night: integer - 0
+    latitude: float - 0.0
+    longitude: float - 0.0
+    amenity_ids: list of string - empty list: it will be the list of Amenity.id later
+    """
+    
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+    
