c2b5a9fe0e168bd295dc87b3a1b3999a236d5cfd
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-02-29 14:39:21 +0100

    file_storage - added FileStorage class with __init__ / all/ new / save / reload methods

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+import json
+import os
+
+
+class FileStorage:
+    def __init__(self):
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        return self.__objects
+
+    def new(self, obj):
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        with open(self.__filename, "w") as newfile:
+            file = json.dumps(self.__objects)
+            newfile.write(file)
+
+    def reload(self):
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as file:
+                self.__objects = json.load(file)
