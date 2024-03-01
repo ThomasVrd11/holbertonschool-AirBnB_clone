3d1625f5c04ff10a25421edc772df05bfe698a29
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-01 12:01:14 +0100

    Added some tests to test_base_models

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,9 +1,34 @@
 import unittest
 from models.base_model import BaseModel
+from datetime import datetime
+import os
 
 
 class TestBaseModel(unittest.TestCase):
+    basemodel = BaseModel()
 
     def test_createBaseModel(self):
-        basemodel = BaseModel()
-        self.assertIsInstance(basemodel, BaseModel)
+        self.assertIsInstance(self.basemodel, BaseModel)
+
+    def test_has_uuid(self):
+        self.assertTrue(self.basemodel.id)
+
+    def test_was_created_at(self):
+        self.assertIsInstance(self.basemodel.created_at, datetime)
+
+    def test_was_updated_at(self):
+        self.assertIsInstance(self.basemodel.updated_at, datetime)
+
+    def test_save_BaseModel(self):
+        self.basemodel.save()
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_BaseModel_to_dict(self):
+        dictionnary = self.basemodel.to_dict()
+        self.assertIsInstance(dictionnary, dict)
+
+    def test_load_BaseModel(self):
+        dictionnary = self.basemodel.to_dict()
+        basemodel2 = BaseModel(**dictionnary)
+        self.assertIsInstance(basemodel2, BaseModel)
+        self.assertTrue(basemodel2.id, self.basemodel.id)
