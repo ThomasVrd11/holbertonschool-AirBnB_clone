bb88351c1beda22401ffaf172eb97a0e005c536a
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-01 14:39:10 +0100

    pycodestyle sweep of tests

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -4,17 +4,18 @@ import unittest
 from models.user import User
 from models.base_model import BaseModel
 
+
 class Test_user(unittest.TestCase):
     """Test the instantation of the User class
     It will also test the class attributes"""
-    
+
     def test_instattion(self):
         user = User()
         """test if inheritance is right coming from BaseModel"""
         self.assertTrue(issubclass(User, BaseModel), True)
         """test if class User is used by user"""
         self.assertIsInstance(user, User)
-        
+
     def test_initialization(self):
         """tests if attributes"""
         user = User()
@@ -23,9 +24,8 @@ class Test_user(unittest.TestCase):
         self.assertIsInstance(user.first_name, str)
         self.assertIsInstance(user.last_name, str)
         # * test if the attributes are empty strings
-        
+
         if __name__ == '__main__':
             unittest.main()
             # * It will run the test if the module is run directly
             # * It will not run if the module is imported
-        
