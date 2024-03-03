ddf3cf2cd0ad25548153d77782a640d02b4d81fc
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-03 21:04:14 +0100

    test_amenity added more tests

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -20,6 +20,27 @@ class TestAmenity(unittest.TestCase):
         amenity = Amenity()
         self.assertIsInstance(amenity.name, str)
 
+    def test_name_default(self):
+        amenity = Amenity()
+        self.assertEqual(amenity.name, "")
+
+    def test_name_assignment(self):
+        amenity = Amenity()
+        amenity.name = "Pool"
+        self.assertEqual(amenity.name, "Pool")
+
+    def test_to_dict_contains_correct_keys(self):
+        amenity = Amenity()
+        amenity.name = "Chong"
+        self.assertIn("name", amenity.to_dict())
+        self.assertIn("id", amenity.to_dict())
+        self.assertIn("created_at", amenity.to_dict())
+        self.assertIn("updated_at", amenity.to_dict())
+
+    def test_to_dict_contains_correct_values_for_name(self):
+        amenity = Amenity()
+        amenity.name = "Tony"
+        self.assertEqual(amenity.to_dict()["name"], "Tony")
 
 if __name__ == '__main__':
     unittest.main()
