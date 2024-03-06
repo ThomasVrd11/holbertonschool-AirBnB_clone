8d3894e5f1932188733e3698ad35dca77e8aefc2
ThomasVrd11 <Thomasviard@orange.fr>
2024-03-06 10:46:58 +0100

    Alex was here

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -6,33 +6,33 @@ import os
 from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models import storage
-
+from models.city import City
 
 
 class TestFileStorage(unittest.TestCase):
     """Unit tests suite for FileStorage class."""
-    model = BaseModel()
 
     def setUp(self):
-        """Create a new instance of FileStorage for each test."""
-        storage = FileStorage()
-        self.file_path = storage._FileStorage__file_path
-        self.obj_dict = storage._FileStorage__objects
+        self.file_path = FileStorage()
+        
 
     def test_instanciates(self):
         """Tests that FileStorage correctly instantiates."""
+        storage = FileStorage()
         self.assertIsInstance(storage, FileStorage)
 
     def test_file_path(self):
         """Test that the file path is a string."""
-        self.assertEqual(type(self.file_path), str)
+        self.assertTrue(type(self.file_path), str)
 
     def test_object(self):
+        storage = FileStorage()
         """Test that __objects is a dictionary."""
-        self.assertEqual(type(self.obj_dict), dict)
+        self.assertEqual(type(storage.all()), dict)
 
     def test_all(self):
         """Test that the all method returns a dictionary."""
+        storage = FileStorage()
         self.obj_dict = {}
         storage.all()
         self.assertEqual(os.path.isfile('file.json'), True)
