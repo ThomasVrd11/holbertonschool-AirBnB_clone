3babbae26ccd6e11656acc7170f2fa7f8f23405a
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-06 11:14:01 +0100

    readding  test one by one to check which break the checkers

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -89,6 +89,8 @@ class TestFileStorage(unittest.TestCase):
             self.storage.all(),
             "User object was not found after reload.")
 
+
+'''
     def test_new_different_objects(self):
         """test that the storage system can handle different objects."""
         user = User()
@@ -107,7 +109,6 @@ class TestFileStorage(unittest.TestCase):
             "State object was not found in storage.")
 
 
-'''
     def test_saving_and_reloading_multiple_objects(self):
         self.storage.save()
         """test that the storage system can handle multiple objects."""
