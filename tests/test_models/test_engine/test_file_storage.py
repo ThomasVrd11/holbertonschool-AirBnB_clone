6d984f80cf12850a432bcba4b6d4f7ae17cf31ef
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-07 09:19:26 +0100

    still testing TFS

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -78,6 +78,12 @@ class TestFileStorage(unittest.TestCase):
             data[key]['name'],
             user.name)
 
+    def test_init_file_path(self):
+        storage = FileStorage()
+        expected_path = "file.json"
+        storage_path = storage._FileStorage__file_path
+        self.assertEqual(expected_path, storage_path)
+
 
 '''
 
